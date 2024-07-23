14067bfbb9ee612c98dae35746a15d369af4d3f6
Turtles009 <sudatts09@gmail.com>
2024-07-23 23:18:42 +0530

    Understand how builder helps in fetching the context, required to notify the listeners of the change correctly.

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,99 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: HomePage(),
+    );
+  }
+}
+
+class SliderData extends ChangeNotifier {
+  double _value = 0.0;
+  double get value => _value;
+  set value(double newValue) {
+    if (_value != newValue) {
+      _value = newValue;
+      notifyListeners();
+    }
+  }
+}
+
+final sliderData = SliderData();
+
+class SliderInheritedNotifier extends InheritedNotifier<SliderData> {
+  const SliderInheritedNotifier({
+    super.key,
+    required super.child,
+    required SliderData sliderData,
+  }) : super(notifier: sliderData);
+
+  static double of(BuildContext context) =>
+      context
+          .dependOnInheritedWidgetOfExactType<SliderInheritedNotifier>()
+          ?.notifier
+          ?.value ??
+      0.0;
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Home Page',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontStyle: FontStyle.italic,
+          ),
+        ),
+      ),
+      body: SliderInheritedNotifier(
+        sliderData: sliderData,
+        child: Builder(
+          builder: (context) {
+            return Column(
+              children: [
+                Slider(
+                  value: SliderInheritedNotifier.of(context),
+                  onChanged: (value) {
+                    sliderData.value = value;
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Opacity(
+                        opacity: SliderInheritedNotifier.of(context),
+                        child: Container(
+                          height: 200,
+                          color: Colors.yellow,
+                        ),
+                      ),
+                      Opacity(
+                        opacity: SliderInheritedNotifier.of(context),
+                        child: Container(
+                          height: 200,
+                          color: Colors.blue,
+                        ),
+                      )
+                    ].expandEqually().toList(),
+                  ),
+                )
+              ],
+            );
+          },
         ),
       ),
     );
   }
+}
+
+extension ExpandEqually on Iterable<Widget> {
+  Iterable<Widget> expandEqually() => map(
+        (w) => Expanded(child: w),
+      );
 }
